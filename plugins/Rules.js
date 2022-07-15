fdad00364b68041f562914538bc50033432ae04b
JakaV7 <108877453+JakaV7@users.noreply.github.com>
2022-07-15 16:09:55 +0700

    Update Rules.js

--- plugins/Rules.js
+++ plugins/Rules.js
@@ -4,29 +4,29 @@ let handler = async(m, { conn }) => {
     let kontol = `Kebijakan Privasi, Syarat Ketentuan dan Peraturan *FACHRIBOT*
 
 *Kebijakan Privasi*
-1. FACHRIBOT tidak akan merekam data riwayat chat user.
-2. FACHRIBOT tidak akan menyebarkan nomor users.
-3. FACHRIBOT tidak akan menyimpan media yang dikirimkan oleh users.
-4. FACHRIBOT tidak akan menyalah gunakan data data users.
-5. Owner FACHRIBOT berhak melihat data riwayat chat users.
-6. Owner FACHRIBOT berhak melihat status users.
-7. Owner FACHRIBOT dapat melihat riwayat chat, dan media yang dikirimkan users.
+1. KinanBOT-MD tidak akan merekam data riwayat chat user.
+2. KinanBOT-MD tidak akan menyebarkan nomor users.
+3. KinanBOT-MD tidak akan menyimpan media yang dikirimkan oleh users.
+4. KinanBOT-MD tidak akan menyalah gunakan data data users.
+5. Owner KinanBOT-MD berhak melihat data riwayat chat users.
+6. Owner KinanBOT-MD berhak melihat status users.
+7. Owner KinanBOT-MD dapat melihat riwayat chat, dan media yang dikirimkan users.
 
-*Peraturan FACHRIBOT*
+*Peraturan KinanBOT-MD*
 1. Users dilarang menelpon maupun memvideo call nomor bot.
 2. Users dilarang mengirimkan berbagai bug, virtex, dll ke nomor bot.
 3. Users diharap tidak melakukan spam dalam penggunaan bot.
 4. Users dilarang menambahkan nomor bot secara illegal, untuk menambahkan silahkan hubungi owner.
 5. Users diharap untuk tidak menyalah gunakan fitur fitur bot.
 
-*Syarat Ketentuan FACHRIBOT*
+*Syarat Ketentuan KinanBOT-MD*
 1. Bot akan keluar dari group apabila sudah waktunya keluar.
-2. FACHRIBOT dapat mem-ban users secara sepihak terlepas dari users salah atau tidak.
-3. FACHRIBOT *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
-4. FACHRIBOT akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
-5. FACHRIBOT bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
+2. KinanBOT-MD dapat mem-ban users secara sepihak terlepas dari users salah atau tidak.
+3. KinanBOT-MD *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
+4. KinanBOT-MD akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
+5. KinanBOT-MD bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
 
-~ Fachri
+~ Jaka
 
 Peraturan: 11/jul/22
 `.trim()
