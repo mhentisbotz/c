89ff2d68a5c6036c2b9429447a5ee7ce228fa2e5
JakaV7 <108877453+JakaV7@users.noreply.github.com>
2022-07-15 16:29:04 +0700

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -5,12 +5,13 @@ let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
 ┌〔 Donasi • Emoney 〕
-├ Dana : 085713041886
-├ Indosat : 08159917553
+├ Dana : 081335334848
+├ Gopay : 085785694474
+├ Telkomsel : 081335334848
 └────
 Berapapun donasi kalian akan sangat berarti 👍
 *Contact person Owner:*
-wa.me/6285713041886 (Fachri)`
+wa.me/6285785694474 (Jaka)`
 conn.sendHydrated(m.chat, str, wm, './media/IMG-20220706-WA0063.jpg', 'https://github.com/Ipulpachri', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m)
