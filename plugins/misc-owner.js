f1bd192f53d35e668cfe4bc07d539725e469c015
JakaV7 <108877453+JakaV7@users.noreply.github.com>
2022-07-15 16:31:56 +0700

    Update misc-owner.js

--- plugins/misc-owner.js
+++ plugins/misc-owner.js
@@ -1,6 +1,6 @@
 function handler(m) {
   let data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  let ftroli = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "status@broadcast"}, "message": {orderMessage: {itemCount: 1,status: 200, surface: 200, message: `                   「 MY OWNER 」            `, orderTitle: 'FachriBot', sellerJid: '0@s.whatsapp.net'}}}
+  let ftroli = {key: {fromMe: false,"participant":"0@s.whatsapp.net", "remoteJid": "status@broadcast"}, "message": {orderMessage: {itemCount: 1,status: 200, surface: 200, message: `                   「 MY OWNER 」            `, orderTitle: 'KinanBOT-MD', sellerJid: '0@s.whatsapp.net'}}}
   this.sendContact(m.chat, data.map(([id, name]) => [id, name]), ftroli)
 }
 handler.alias = ['owner']
