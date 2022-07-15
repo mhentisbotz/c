449cd16299ea1c653f6fdd7db1b379791ce9575d
JakaV7 <108877453+JakaV7@users.noreply.github.com>
2022-07-15 16:29:59 +0700

    Update info-gcbott.js

--- plugins/info-gcbott.js
+++ plugins/info-gcbott.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 
-let str = `JOIN DONG\nhttps://chat.whatsapp.com/GimZmhA2XTQFfPXk4XMrv6\n_SEMOGA BETAH_`
+let str = `JOIN DONG\nhttps://chat.whatsapp.com/ED7th5Ie38eBayZEbDPxwY\n_SEMOGA BETAH_`
 conn.sendButtonDoc(m.chat, str, wm, 'Owner','.owner', ftextt, m)
 }
 handler.command = handler.help = ['gcbott']
