acda56a4c28df5419dc31e29fbaf8dc05d42f96b
JakaV08 <109280681+JakaV08@users.noreply.github.com>
2022-07-17 19:57:40 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -11,14 +11,13 @@ let str = `Woy ${conn.getName(m.sender)} Mau main bot?
 JOIN JUGA NIH 
 https://chat.whatsapp.com/ED7th5Ie38eBayZEbDPxwY
 BIAR RAME
-*•SEWA BOT 10K MANEN HUBUNGI OWNER•*
 *Langsung Pencet List Menu!*`
-conn.sendHydrated(m.chat, str, wm, './media/sunrise.jpg', 'https://wa.me/6285785694474', 'Chat owner', null, null, [
-['List Menu', '/m']
+conn.sendHydrated(m.chat, str, wm, './media/ayang.jpg', 'https://Instagram.com/nan_sakrn', 'Follow Gblk', '085785694474', 'Nomor Owner', [
+['Donasi', '/donasi'], ['Owner', '/owner'], ['List Menu', '/m']
 ], m)
 
 }
-handler.help = ['Menu']
+handler.help = ['menu']
 handler.tags = ['info']
 handler.command = /^(menu)$/i
 
