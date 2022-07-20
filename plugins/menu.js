ca4f1adec9b09d9c509851e82706e6b9a256529e
mhentisbotz <109332967+mhentisbotz@users.noreply.github.com>
2022-07-20 10:23:28 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -2,20 +2,15 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
-let str = `*Ready nokos all apk & all Negara*
-Nokos indo _5k_ buat WhatsApp 
-Sekarang cuma *4k*
-Promo terbatas!
-Negara lain?
-Chat saja di nomor ini
-https://wa.me/message/MKYNWO66LFYPL1
-//Yang tidak tau apa itu nokos
-Nokos adalah nomor kosong yang di jual untuk di daftarkan ke apk seperti WhatsApp, telegram, nanti jika kamu beli nokos di saya, saya akan kasih nomor, dan kamu harus daftarin/verifikasi ke WhatsApp/apk lain, dan nanti saya kasih code nya buat Verifikasi!
-_*Jadi tidak perlu beli kartu untuk mendapatkan nomor baru*_
-*Kurang paham?, Jika masih tidak paham tanya saja langsung ke saya*
-Klik https://wa.me/message/MKYNWO66LFYPL1`
-conn.sendHydrated(m.chat, str, wm, './media/ayang.jpg', 'https://Instagram.com/nan_sakrn', 'IG Owner', '085785694474', 'Nomor Owner', [
-['Donasi', '/donasi'], ['Owner', '/owner'], ['List Menu', '/m']
+let str = `*「 MikAzuBOT-MD 」*
+*❒WEB :* 
+●https://is.gd/4ZPpIt
+*❒YT :* 
+●https://bit.ly/3c0WJz9
+*◘IG :* 
+●https://instagram.com/asyrafsixyouu`
+conn.sendHydrated(m.chat, str, wm, './media/ayang.jpg', 'https://is.gd/4ZPpIt', 'WEBSITE OWNER', 'https://bit.ly/3ax9GjJ', 'GROUP BOT', [
+['💰DONASI💰', '/donasi'], ['🌹OWNER😎', '/owner'], ['😱MENUNYA MANA😒', '/m']
 ], m)
 
 }
