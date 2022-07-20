b35f67bb4c220cb28e629af57ee1b6de4a53465e
mhentisbotz <109332967+mhentisbotz@users.noreply.github.com>
2022-07-20 10:11:10 +0700

    Update info-bioowner.js

--- plugins/info-bioowner.js
+++ plugins/info-bioowner.js
@@ -1,20 +1,19 @@
 let handler = async (m, { conn }) => {
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `*───────[ BIODATA OWNER ]───────*
-*✉️ Nama* : Jaka
+*✉️ Nama* : Asyraf
 *♂️ Gender* : Laki - laki
 *🕋 Agama* : Islam
-*⏰ Tanggal lahir* : 20.Juni.2006
-*🎨 Umur* : 16
+*⏰ Tanggal lahir* : 30.September.2006
+*🎨 Umur* : 17
 *🧮 Kelas* : 1 Sma
-*🧩 Hobby* : Chatting, Recode script bbot
+*🧩 Hobby* : Chatting, Recode script BOT, Main Game
 *🗺️ Tinggal* : Gresik, jawa timur, Indonesia
 
 *───────[ SOSIAL MEDIA ]───────*
-*📷 instagran* : @nan_sakrn
-*🇫  Facebook* : @Jaka nation
-*🏮 Chanel Youtube* : I AM NITSU
-*🐈 Github:* JakaV1
+*📷 instagran* : https://is.gd/4ZPpIt
+*🏮 Chanel Youtube* : I AM NITSUhttps://is.gd/4ZPpIt
+*🐈 Github:* https://is.gd/4ZPpIt
 
 `
 conn.sendHydrated(m.chat, teksbio, wm, ppown, "https://wa.me/" + nomorown, "💬 ᴄʜᴀᴛs", null,null, [["ᴅᴏɴᴀsɪ", '.donasi'], [null, null],[null,null]], fkontak)
