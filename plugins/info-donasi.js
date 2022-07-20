351fcd86b914cd3d4fef182f31024e461af1aad5
mhentisbotz <109332967+mhentisbotz@users.noreply.github.com>
2022-07-20 10:35:40 +0700

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -5,15 +5,16 @@ let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
 ┌〔 Donasi • Emoney 〕
-├ Dana : 081335334848
-├ Gopay : 085785694474
-├ Telkomsel : 081335334848
+├ XL : 081946945315
+├ Dana : 085646606905
+├ Gopay : 081946945315
+├ Telkomsel : 085646606905
 └────
 Berapapun donasi kalian akan sangat berarti 👍
 *Contact person Owner:*
-wa.me/6285785694474 (Jaka)`
-conn.sendHydrated(m.chat, str, wm, './media/IMG-20220706-WA0063.jpg', 'https://github.com/Ipulpachri', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
-['𝙼𝙴𝙽𝚄', '/menu']
+wa.me/628 (Jaka)`
+conn.sendHydrated(m.chat, str, wm, './media/IMG-20220706-WA0063.jpg', 'https://is.gd/4ZPpIt', 'WEBSITE', null, null, [
+['MENU MIKAZU', '/menu']
 ], m)
 
 }
